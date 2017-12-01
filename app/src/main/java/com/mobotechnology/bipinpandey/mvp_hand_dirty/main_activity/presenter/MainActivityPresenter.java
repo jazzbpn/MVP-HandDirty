5b578b879bbda136bd656df5d900fd83f830f030
dev_bpn <jazzbpn@gmail.com>
2017-12-01 11:16:10 +0545

    minor fixings

--- app/src/main/java/com/mobotechnology/bipinpandey/mvp_hand_dirty/main_activity/presenter/MainActivityPresenter.java
+++ app/src/main/java/com/mobotechnology/bipinpandey/mvp_hand_dirty/main_activity/presenter/MainActivityPresenter.java
@@ -4,6 +4,10 @@ import com.mobotechnology.bipinpandey.mvp_hand_dirty.main_activity.model.User;
 
 /**
  * Created by bpn on 11/30/17.
+ *
+ * 0. In MVP the presenter assumes the functionality of the "middle-man". All presentation logic is pushed to the presenter.
+ * 1. Listens to user action and model updates
+ * 2. Updates model and view
  */
 
 public class MainActivityPresenter {
